3c5f2cc0f35e342f979a994af5d11c53455fec6b
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-02-02 19:54:36 +0000

    app.rb

--- app.rb
+++ app.rb
@@ -2,8 +2,38 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  play_game("Rock")
+end
+
+get("/paper") do
+  play_game("Paper")
+end
+
+get("/scissors") do
+  play_game("Scissors")
+end
+
+def play_game(player_choice)
+  @player_choice = player_choice
+  @computer_choice = %w(Rock Paper Scissors).sample
+  @result = determine_winner(@player_choice, @computer_choice)
+
+  erb(:result)
+end
+
+def determine_winner(player, computer)
+  return "\nWe tied!" if player == computer
+
+  case player
+  when "Rock"
+    return (computer == "Scissors") ? "We won!" : "We lost!"
+  when "Paper"
+    return (computer == "Rock") ? "We won!" : "We lost!"
+  when "Scissors"
+    return (computer == "Paper") ? "We won!" : "We lost!"
+  end
 end
